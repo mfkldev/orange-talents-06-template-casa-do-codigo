1359ff05dbf463bbbfc33f657ca62f72d6e0df81
mfkldev <marcio.souza@zup.com.br>
2021-06-29 09:36:32 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupacademy/marciosouza/cadadocodigo/CadaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.marciosouza.cadadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadaDoCodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/marciosouza/cadadocodigo/CadaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.marciosouza.cadadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
